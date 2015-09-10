e607c8f7240b11f180028446744549aaaacba27b
Bharathkumar <bharath2k2in@gmail.com>
2015-09-10 21:19:57 +0530

    reverted the last commit.
    back to gson mapper instead of jackson

--- src/main/java/com/ibm/cloudoe/samples/service/ProductSalesService.java
+++ src/main/java/com/ibm/cloudoe/samples/service/ProductSalesService.java
@@ -11,8 +11,6 @@ import javax.ws.rs.core.MediaType;
 
 import org.apache.commons.io.IOUtils;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.ibm.cloudoe.samples.service.domain.Account;
 import com.ibm.cloudoe.samples.service.domain.FormRequest;
@@ -20,16 +18,12 @@ import com.ibm.cloudoe.samples.service.domain.FormRequest;
 @Path("/products")
 public class ProductSalesService {
 
-
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Account getProducts(final FormRequest request) throws URISyntaxException, IOException {
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         String accounts = IOUtils.toString(classloader.getResourceAsStream("account.json"));
-        ObjectMapper mapper = new ObjectMapper();
-        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-        return mapper.readValue(accounts, Account.class);
-//        return new Gson().fromJson(accounts, Account.class);
+        return new Gson().fromJson(accounts, Account.class);
     }
 }
